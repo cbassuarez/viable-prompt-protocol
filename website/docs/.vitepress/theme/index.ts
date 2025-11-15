87c80999a238d4a7f76c55ea72ec35b8d5182430
Stage Devices <developer@stagedevices.com>
2025-11-15 10:52:56 -0800

    Revert to ae2d544f (undo later commits)

--- website/docs/.vitepress/theme/index.ts
+++ website/docs/.vitepress/theme/index.ts
@@ -1,56 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
-import 'katex/dist/katex.min.css';
 import './style.css';
 
-const theme: Theme = {
-  ...DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp?.(ctx);
-
-    if (typeof window === 'undefined') {
-      return;
-    }
-
-    const { router } = ctx;
-    const originalAfterRouteChanged = router.onAfterRouteChanged?.bind(router);
-
-    const typesetMath = () => {
-      const mathjax = (window as typeof window & { MathJax?: any }).MathJax;
-
-      if (!mathjax) {
-        return;
-      }
-
-      if (typeof mathjax.typesetClear === 'function') {
-        mathjax.typesetClear();
-      }
-
-      if (typeof mathjax.typesetPromise === 'function') {
-        mathjax.typesetPromise().catch(() => {
-          /* swallow MathJax promise errors */
-        });
-      } else if (typeof mathjax.typeset === 'function') {
-        mathjax.typeset();
-      }
-    };
-
-    const queueTypeset = () => {
-      requestAnimationFrame(() => {
-        queueMicrotask(typesetMath);
-      });
-    };
-
-    if (document.readyState === 'complete') {
-      queueTypeset();
-    } else {
-      window.addEventListener('load', queueTypeset, { once: true });
-    }
-
-    router.onAfterRouteChanged = (...args) => {
-      originalAfterRouteChanged?.(...args);
-      queueTypeset();
-    };
-  }
+export default {
+  ...DefaultTheme
 };
-
-export default theme;
