1bbaebbf675dc8bc2009a7517cd6e8ed66375cc9
seb suarez <162302050+cbassuarez@users.noreply.github.com>
2025-12-16 19:07:17 -0800

    Update Layout.vue

--- website/docs/.vitepress/theme/Layout.vue
+++ website/docs/.vitepress/theme/Layout.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="vpp-layout">
-    <div class="vpp-announcement-bar">
-      <span class="vpp-announcement-label">VPPChat beta is live.</span>
-      <a class="vpp-announcement-link" href="https://testflight.apple.com/join/EXAMPLE">
-        Join on TestFlight →
-      </a>
-    </div>
     <transition name="vpp-page-fade" mode="out-in">
       <Layout :key="route.path" />
     </transition>
