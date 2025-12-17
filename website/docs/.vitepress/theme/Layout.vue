2337921a96638180e33172059710d51851d1d52f
seb suarez <162302050+cbassuarez@users.noreply.github.com>
2025-12-16 18:49:40 -0800

    Polish site motion and glass interactions

--- website/docs/.vitepress/theme/Layout.vue
+++ website/docs/.vitepress/theme/Layout.vue
@@ -6,12 +6,16 @@
         Join on TestFlight →
       </a>
     </div>
-    <Layout />
+    <transition name="vpp-page-fade" mode="out-in">
+      <Layout :key="route.path" />
+    </transition>
   </div>
 </template>
 
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
+import { useRoute } from 'vitepress';
 
 const { Layout } = DefaultTheme;
+const route = useRoute();
 </script>
